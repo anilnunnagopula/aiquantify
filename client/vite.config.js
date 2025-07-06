32643ee13bfe343d3d42ba4bc4cbab8e2b970ebc
anilnunnagopula <btech2k26@gmail.com>
2025-07-07 00:24:38 +0530

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "", // << MAGIC SETTING for Netlify and static hosting
+  base: "./", // ← critical for Netlify/Vercel if using assets
 });
+
