28d59a21127107ca29fb87c48a5da38946c1cfd2
anilnunnagopula <btech2k26@gmail.com>
2025-07-07 01:08:46 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // just let Netlify resolve paths normally
+  base: "/", // ✅ THIS IS KEY
   build: {
     outDir: "dist",
   },
